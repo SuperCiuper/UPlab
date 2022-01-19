e4e6fdd221970381cde39595e3654067c20d4b15
SuperCiuper <szymo1212@wp.pl>
2022-01-19 12:30:55 +0100

    lab6-bluetooth

---
+++ UPCode/Lab6-bluetooth/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace Lab6_bluetooth
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelDiscoverDevices = new System.Windows.Forms.Label();
+            this.labelConnection = new System.Windows.Forms.Label();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.buttonDiscoverDevices = new System.Windows.Forms.Button();
+            this.listBoxDiscoveredDevices = new System.Windows.Forms.ListBox();
+            this.buttonConnection = new System.Windows.Forms.Button();
+            this.listBoxConnected = new System.Windows.Forms.ListBox();
+            this.richTextBoxInfo = new System.Windows.Forms.RichTextBox();
+            this.buttonSelectFile = new System.Windows.Forms.Button();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxSelectedFile = new System.Windows.Forms.TextBox();
+            this.buttonShowConnectors = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelDiscoverDevices
+            // 
+            this.labelDiscoverDevices.AutoSize = true;
+            this.labelDiscoverDevices.Location = new System.Drawing.Point(9, 88);
+            this.labelDiscoverDevices.Name = "labelDiscoverDevices";
+            this.labelDiscoverDevices.Size = new System.Drawing.Size(101, 13);
+            this.labelDiscoverDevices.TabIndex = 0;
+            this.labelDiscoverDevices.Text = "Discovered devices";
+            // 
+            // labelConnection
+            // 
+            this.labelConnection.AutoSize = true;
+            this.labelConnection.Location = new System.Drawing.Point(9, 216);
+            this.labelConnection.Name = "labelConnection";
+            this.labelConnection.Size = new System.Drawing.Size(81, 13);
+            this.labelConnection.TabIndex = 1;
+            this.labelConnection.Text = "Connected with";
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(9, 399);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(25, 13);
+            this.labelInfo.TabIndex = 2;
+            this.labelInfo.Text = "Info";
+            // 
+            // buttonDiscoverDevices
+            // 
+            this.buttonDiscoverDevices.Location = new System.Drawing.Point(116, 78);
+            this.buttonDiscoverDevices.Name = "buttonDiscoverDevices";
+            this.buttonDiscoverDevices.Size = new System.Drawing.Size(131, 23);
+            this.buttonDiscoverDevices.TabIndex = 3;
+            this.buttonDiscoverDevices.Text = "Discover devices";
+            this.buttonDiscoverDevices.UseVisualStyleBackColor = true;
+            this.buttonDiscoverDevices.Click += new System.EventHandler(this.buttonDiscoverDevices_Click);
+            // 
+            // listBoxDiscoveredDevices
+            // 
+            this.listBoxDiscoveredDevices.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBoxDiscoveredDevices.FormattingEnabled = true;
+            this.listBoxDiscoveredDevices.Location = new System.Drawing.Point(9, 107);
+            this.listBoxDiscoveredDevices.Name = "listBoxDiscoveredDevices";
+            this.listBoxDiscoveredDevices.Size = new System.Drawing.Size(238, 93);
+            this.listBoxDiscoveredDevices.TabIndex = 4;
+            this.listBoxDiscoveredDevices.Tag = "";
+            // 
+            // buttonConnection
+            // 
+            this.buttonConnection.Location = new System.Drawing.Point(116, 206);
+            this.buttonConnection.Name = "buttonConnection";
+            this.buttonConnection.Size = new System.Drawing.Size(131, 23);
+            this.buttonConnection.TabIndex = 5;
+            this.buttonConnection.Text = "Connect";
+            this.buttonConnection.UseVisualStyleBackColor = true;
+            this.buttonConnection.Click += new System.EventHandler(this.buttonConnection_Click);
+            // 
+            // listBoxConnected
+            // 
+            this.listBoxConnected.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBoxConnected.FormattingEnabled = true;
+            this.listBoxConnected.Location = new System.Drawing.Point(9, 235);
+            this.listBoxConnected.Name = "listBoxConnected";
+            this.listBoxConnected.Size = new System.Drawing.Size(238, 93);
+            this.listBoxConnected.TabIndex = 6;
+            this.listBoxConnected.Tag = "";
+            // 
+            // richTextBoxInfo
+            // 
+            this.richTextBoxInfo.Location = new System.Drawing.Point(12, 418);
+            this.richTextBoxInfo.Name = "richTextBoxInfo";
+            this.richTextBoxInfo.ReadOnly = true;
+            this.richTextBoxInfo.Size = new System.Drawing.Size(238, 231);
+            this.richTextBoxInfo.TabIndex = 7;
+            this.richTextBoxInfo.Text = "";
+            this.richTextBoxInfo.TextChanged += new System.EventHandler(this.richTextBoxInfo_TextChanged);
+            // 
+            // buttonSelectFile
+            // 
+            this.buttonSelectFile.Location = new System.Drawing.Point(9, 334);
+            this.buttonSelectFile.Name = "buttonSelectFile";
+            this.buttonSelectFile.Size = new System.Drawing.Size(119, 23);
+            this.buttonSelectFile.TabIndex = 8;
+            this.buttonSelectFile.Text = "Select file";
+            this.buttonSelectFile.UseVisualStyleBackColor = true;
+            this.buttonSelectFile.Click += new System.EventHandler(this.buttonSelectFile_Click);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(134, 334);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(113, 23);
+            this.buttonSend.TabIndex = 9;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxSelectedFile
+            // 
+            this.textBoxSelectedFile.Location = new System.Drawing.Point(9, 363);
+            this.textBoxSelectedFile.Name = "textBoxSelectedFile";
+            this.textBoxSelectedFile.ReadOnly = true;
+            this.textBoxSelectedFile.Size = new System.Drawing.Size(238, 20);
+            this.textBoxSelectedFile.TabIndex = 10;
+            this.textBoxSelectedFile.Text = "No file selected";
+            // 
+            // buttonShowConnectors
+            // 
+            this.buttonShowConnectors.Location = new System.Drawing.Point(116, 49);
+            this.buttonShowConnectors.Name = "buttonShowConnectors";
+            this.buttonShowConnectors.Size = new System.Drawing.Size(131, 23);
+            this.buttonShowConnectors.TabIndex = 11;
+            this.buttonShowConnectors.Text = "Show connectors";
+            this.buttonShowConnectors.UseVisualStyleBackColor = true;
+            this.buttonShowConnectors.Click += new System.EventHandler(this.buttonShowConnectors_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 661);
+            this.Controls.Add(this.buttonShowConnectors);
+            this.Controls.Add(this.textBoxSelectedFile);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.buttonSelectFile);
+            this.Controls.Add(this.richTextBoxInfo);
+            this.Controls.Add(this.listBoxConnected);
+            this.Controls.Add(this.buttonConnection);
+            this.Controls.Add(this.listBoxDiscoveredDevices);
+            this.Controls.Add(this.buttonDiscoverDevices);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.labelConnection);
+            this.Controls.Add(this.labelDiscoverDevices);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDiscoverDevices;
+        private System.Windows.Forms.Label labelConnection;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Button buttonDiscoverDevices;
+        private System.Windows.Forms.ListBox listBoxDiscoveredDevices;
+        private System.Windows.Forms.Button buttonConnection;
+        private System.Windows.Forms.ListBox listBoxConnected;
+        private System.Windows.Forms.RichTextBox richTextBoxInfo;
+        private System.Windows.Forms.Button buttonSelectFile;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxSelectedFile;
+        private System.Windows.Forms.Button buttonShowConnectors;
+    }
+}
+
---
+++ UPCode/Lab6-bluetooth/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using System.Threading; 
+
+namespace Lab6_bluetooth
+{
+    public partial class Form1 : Form
+    {
+        public const string DEVICE_PIN = "000000";
+        private bool continueScanning = false;
+        private bool isPaired = false;
+        private bool isUnpaired = false;
+        private OpenFileDialog openFile = new OpenFileDialog();
+        private List<BluetoothDeviceInfo> devices = new List<BluetoothDeviceInfo>();
+        BluetoothDeviceInfo deviceToPair = null;
+        private string[] fileNames; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ScanDevices()
+        {
+            try
+            {
+                while (continueScanning)
+                {
+                    var bluetoothClient = new BluetoothClient();
+                    var bluetoothDevices = bluetoothClient.DiscoverDevices();
+
+                    foreach (var bluetoothDevice in bluetoothDevices)
+                    {
+                        richTextBoxInfo.AppendText(string.Format("Nazwa urządzenia: {0}\r\nTyp urządzenia: {1} Połączono: {2} Adres: {3}\r\nOstatnio widziano: {4} Ostatnio użyto: {5}\n",
+                            bluetoothDevice.DeviceName, bluetoothDevice.ClassOfDevice.Device, bluetoothDevice.Connected, bluetoothDevice.DeviceAddress, bluetoothDevice.LastSeen, bluetoothDevice.LastUsed));
+
+                        bool onList = false;
+
+                        if (bluetoothDevice.DeviceName != null)
+                        {
+                            foreach (string item in listBoxDiscoveredDevices.Items)
+                                if (item == bluetoothDevice.DeviceName)
+                                    onList = true;
+                        }
+
+                        if (!onList)
+                        {
+                            listBoxDiscoveredDevices.Items.Add(bluetoothDevice.DeviceName);
+                            devices.Add(bluetoothDevice);
+                        }
+                    }
+                }
+            } catch (Exception error)
+            {
+                richTextBoxInfo.AppendText("ERROR: \n" + error.Message + "\n");
+            }
+
+        }
+
+        private void buttonDiscoverDevices_Click(object sender, EventArgs e)
+        {
+            if (buttonDiscoverDevices.Text == "Discover devices")
+            {
+                buttonDiscoverDevices.Text = "Stop discovering";
+                continueScanning = true;
+                richTextBoxInfo.AppendText("Discovering devices\n");
+            }
+            else
+            {
+                buttonDiscoverDevices.Text = "Discover devices";
+                continueScanning = true;
+                richTextBoxInfo.AppendText("Stopped discovering\n");
+            }
+            Thread findDevices = new Thread(ScanDevices);
+            findDevices.Priority = ThreadPriority.Highest;
+            findDevices.IsBackground = true;
+            findDevices.Start();
+
+        }
+
+        private void ReceiveFiles()
+        {
+            while (isPaired)
+            {
+                var listener = new ObexListener(ObexTransport.Bluetooth);
+                listener.Start();
+                ObexListenerContext ctx = listener.GetContext();
+                ObexListenerRequest req = ctx.Request;
+                String[] pathSplits = req.RawUrl.Split('/');
+                String file = pathSplits[pathSplits.Length - 1];
+                req.WriteFile(file);
+                richTextBoxInfo.AppendText("File received\n");
+                listener.Stop();
+            }
+        }
+
+        private void ConnectWithDevice()
+        {
+            foreach (var device in devices)
+            {
+                if (device.DeviceName == (string)listBoxDiscoveredDevices.SelectedItem)
+                    deviceToPair = device;
+            }
+
+            if (deviceToPair == null)
+            {
+                richTextBoxInfo.AppendText("Select device from Discovered devices list\n");
+            }
+            else
+            {
+                richTextBoxInfo.AppendText("Connecting with: " + (string)deviceToPair.DeviceName + "\n");
+                deviceToPair.Update();
+                deviceToPair.Refresh();
+                deviceToPair.SetServiceState(BluetoothService.ObexObjectPush, true);
+                isPaired = BluetoothSecurity.PairRequest(deviceToPair.DeviceAddress, DEVICE_PIN);
+                if (isPaired)
+                {
+                    buttonConnection.Text = "Disconnect";
+                    richTextBoxInfo.AppendText("Connected\n");
+                    listBoxConnected.Items.Add(deviceToPair.DeviceName);
+
+                    Thread receiveFiles = new Thread(ReceiveFiles);
+                    receiveFiles.Priority = ThreadPriority.Highest;
+                    receiveFiles.IsBackground = true;
+                    receiveFiles.Start();
+                }
+                else
+                {
+                    richTextBoxInfo.AppendText("Couldn't connect with the device\n");
+                }
+            }
+        }
+
+        private void UnpairDevice()
+        {
+            if (deviceToPair.DeviceName == listBoxConnected.SelectedItem.ToString())
+                isUnpaired = BluetoothSecurity.RemoveDevice(deviceToPair.DeviceAddress);
+
+            if (isUnpaired)
+            {
+                buttonConnection.Text = "Connect";
+                richTextBoxInfo.AppendText("Disconnected\n");
+                listBoxConnected.Items.Remove(deviceToPair.DeviceName);
+                isPaired = false;
+            }
+            else
+            {
+                richTextBoxInfo.AppendText("Couldn't disconnect the device\n");
+            }
+        }
+
+        private void buttonConnection_Click(object sender, EventArgs e)
+        {
+            if (buttonConnection.Text == "Connect")
+            {
+                Thread pairWithDevice = new Thread(ConnectWithDevice);
+                pairWithDevice.Priority = ThreadPriority.Highest;
+                pairWithDevice.IsBackground = true;
+                pairWithDevice.Start();
+            }
+            else
+            {
+                UnpairDevice();
+
+            }
+
+        }
+
+        private void buttonShowConnectors_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBoxInfo_TextChanged(object sender, EventArgs e)
+        {
+            richTextBoxInfo.SelectionStart = richTextBoxInfo.Text.Length;
+            richTextBoxInfo.ScrollToCaret();
+        }
+
+        private void buttonSelectFile_Click(object sender, EventArgs e)
+        {
+            openFile.ShowDialog();
+            fileNames = openFile.FileNames;
+        }
+        private void Send()
+        {
+            foreach (string file in fileNames)
+            {
+                try
+                {
+                    if (deviceToPair != null)
+                        richTextBoxInfo.AppendText("Sending the file\n");
+
+                    var uri = new Uri("obex://" + deviceToPair.DeviceAddress + "/" + file);
+                    var request = new ObexWebRequest(uri);
+                    request.ReadFile(file);
+                    var response = (ObexWebResponse)request.GetResponse();
+                    richTextBoxInfo.AppendText("File send successfully " + file + "\n");
+                }
+                catch (Exception error)
+                {
+                    richTextBoxInfo.AppendText("ERROR: " + error.Message + "\n");
+                }
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            Thread sendFilesTh = new Thread(Send);
+            sendFilesTh.Priority = ThreadPriority.Highest;
+            sendFilesTh.IsBackground = true;
+            sendFilesTh.Start();
+        }
+    }
+}
