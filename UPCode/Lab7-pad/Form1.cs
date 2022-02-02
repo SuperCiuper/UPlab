a7dd709b92dd61931829b8fced3c13c6d5db8249
SuperCiuper <szymo1212@wp.pl>
2022-02-02 13:51:25 +0100

    lab7- final

--- UPCode/Lab7-pad/Form1.cs
+++ UPCode/Lab7-pad/Form1.cs
@@ -19,7 +19,7 @@ namespace Lab7_pad {
         private bool painting = true;
         private Graphics graphics;
         private SolidBrush solidBrush;
-        private int width = 5;
+        private int width = 6;
         private int zeroPoint = 32767;
         private int xVal = 32767;
         private int yVal = 32767;
@@ -57,21 +57,23 @@ namespace Lab7_pad {
 
         private void timer_Tick(object sender, EventArgs e) {
             pollJoystick();
-            //richTextBoxConsole.AppendText(Cursor.Position.X.ToString() + " " + Cursor.Position.Y.ToString());
-            //solidBrush = new SolidBrush(Color.FromArgb(255, Convert.ToInt32(numericUpDownRed.Value), Convert.ToInt32(numericUpDownGreen.Value), Convert.ToInt32(numericUpDownBlue.Value)));
         }
 
         private void pollJoystick() {
             JoystickState state = new JoystickState();
             state = gamepad.GetCurrentState();
 
-            if (state.X == zeroPoint)
+
+            if (state.X == zeroPoint) {
                 xVal = 0;
-
-            else if (state.X < zeroPoint)
+                trackBarX.Value = 50;
+            } else if (state.X < zeroPoint) {
                 xVal = -2;
-
-            else xVal = 2;
+                trackBarX.Value = 0;
+            } else {
+                xVal = 2;
+                trackBarX.Value = 100;
+            }
 
             if (state.Y == zeroPoint)
                 yVal = 0;
@@ -81,16 +83,8 @@ namespace Lab7_pad {
 
             else yVal = 2;
 
-            MouseMove(xVal, yVal);
-
-            textBoxX.Text = xVal.ToString();
-            textBoxY.Text = yVal.ToString();
-
-            if (xVal < oldXVal) {
-                trackBarX.Value--;
-            } else if (xVal > oldXVal) {
-                trackBarX.Value++;
-            }
+            textBoxX.Text = state.X.ToString();
+            textBoxY.Text = state.Y.ToString();
 
             if (state.Buttons[0] && lastButtonSelected != 0) {
                 radioButtonBlue.Checked = true;
@@ -116,16 +110,46 @@ namespace Lab7_pad {
                 painting = false;
             }
 
+            if (state.Buttons[4]) {
+                if (radioButtonBlue.Checked) {
+                    if (numericUpDownBlue.Value < 251) numericUpDownBlue.Value += 5;
+                }
+                if (radioButtonRed.Checked) {
+                    if (numericUpDownRed.Value < 251) numericUpDownRed.Value += 5;
+                }
+                if (radioButtonGreen.Checked) {
+                    if (numericUpDownGreen.Value < 251) numericUpDownGreen.Value += 5;
+                }
+            }
+
+            if (state.Buttons[5]) {
+                if (radioButtonBlue.Checked) {
+                    if (numericUpDownBlue.Value > 4) numericUpDownBlue.Value -= 5;
+                }
+                if (radioButtonRed.Checked) {
+                    if (numericUpDownRed.Value > 4) numericUpDownRed.Value -= 5;
+                }
+                if (radioButtonGreen.Checked) {
+                    if (numericUpDownGreen.Value > 4) numericUpDownGreen.Value -= 5;
+                }
+            }
+
+            if (state.Buttons[6]) {
+                panelCanvas.Invalidate();
+            }
+
             solidBrush = new SolidBrush(Color.FromArgb(255, Convert.ToInt32(numericUpDownRed.Value), Convert.ToInt32(numericUpDownGreen.Value), Convert.ToInt32(numericUpDownBlue.Value)));
+            moveMouse(xVal, yVal);
         }
 
-        public void MouseMove(int posx, int posy) {
-            Cursor.Position = new Point(Cursor.Position.X + posx, Cursor.Position.Y + posy);
+        public void moveMouse(int posx, int posy) {
+            Cursor.Position = new Point(Cursor.Position.X + (posx * 2), Cursor.Position.Y + (posy * 2));
             Cursor.Clip = new Rectangle(this.Location, this.Size);
         }
 
         private void panelCanvas_MouseMove(object sender, MouseEventArgs e) {
             if (painting) {
+                //richTextBoxConsole.AppendText("XD ");
                 graphics = panelCanvas.CreateGraphics();
                 graphics.FillEllipse(solidBrush, e.X, e.Y, width, width);
             }
