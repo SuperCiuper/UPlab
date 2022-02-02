d53a39c82282321f31b9b975008cc2b151535c09
SuperCiuper <szymo1212@wp.pl>
2022-02-02 12:15:15 +0100

    lab7 - start

---
+++ UPCode/Lab7-pad/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿
+namespace Lab7_pad {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBoxConsole = new System.Windows.Forms.RichTextBox();
+            this.labelConsole = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.trackBarX = new System.Windows.Forms.TrackBar();
+            this.panelDrawing = new System.Windows.Forms.Panel();
+            this.numericUpDownBlue = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownGreen = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownRed = new System.Windows.Forms.NumericUpDown();
+            this.labelColor = new System.Windows.Forms.Label();
+            this.panelCanvas = new System.Windows.Forms.Panel();
+            this.radioButtonRed = new System.Windows.Forms.RadioButton();
+            this.radioButtonGreen = new System.Windows.Forms.RadioButton();
+            this.radioButtonBlue = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarX)).BeginInit();
+            this.panelDrawing.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBlue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBoxConsole
+            // 
+            this.richTextBoxConsole.Location = new System.Drawing.Point(20, 47);
+            this.richTextBoxConsole.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBoxConsole.Name = "richTextBoxConsole";
+            this.richTextBoxConsole.Size = new System.Drawing.Size(217, 107);
+            this.richTextBoxConsole.TabIndex = 0;
+            this.richTextBoxConsole.Text = "";
+            // 
+            // labelConsole
+            // 
+            this.labelConsole.AutoSize = true;
+            this.labelConsole.Location = new System.Drawing.Point(18, 24);
+            this.labelConsole.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelConsole.Name = "labelConsole";
+            this.labelConsole.Size = new System.Drawing.Size(65, 13);
+            this.labelConsole.TabIndex = 1;
+            this.labelConsole.Text = "Komunikaty:";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(352, 47);
+            this.textBoxX.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(76, 20);
+            this.textBoxX.TabIndex = 2;
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(352, 70);
+            this.textBoxY.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(76, 20);
+            this.textBoxY.TabIndex = 3;
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Location = new System.Drawing.Point(310, 52);
+            this.labelX.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(36, 13);
+            this.labelX.TabIndex = 4;
+            this.labelX.Text = "labelX";
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Location = new System.Drawing.Point(310, 70);
+            this.labelY.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(36, 13);
+            this.labelY.TabIndex = 5;
+            this.labelY.Text = "labelY";
+            // 
+            // trackBarX
+            // 
+            this.trackBarX.Location = new System.Drawing.Point(274, 109);
+            this.trackBarX.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBarX.Maximum = 100;
+            this.trackBarX.Name = "trackBarX";
+            this.trackBarX.Size = new System.Drawing.Size(238, 45);
+            this.trackBarX.TabIndex = 6;
+            this.trackBarX.Value = 50;
+            // 
+            // panelDrawing
+            // 
+            this.panelDrawing.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panelDrawing.Controls.Add(this.radioButtonBlue);
+            this.panelDrawing.Controls.Add(this.radioButtonGreen);
+            this.panelDrawing.Controls.Add(this.radioButtonRed);
+            this.panelDrawing.Controls.Add(this.numericUpDownBlue);
+            this.panelDrawing.Controls.Add(this.numericUpDownGreen);
+            this.panelDrawing.Controls.Add(this.numericUpDownRed);
+            this.panelDrawing.Controls.Add(this.labelColor);
+            this.panelDrawing.Controls.Add(this.panelCanvas);
+            this.panelDrawing.Location = new System.Drawing.Point(12, 159);
+            this.panelDrawing.Name = "panelDrawing";
+            this.panelDrawing.Size = new System.Drawing.Size(576, 411);
+            this.panelDrawing.TabIndex = 7;
+            // 
+            // numericUpDownBlue
+            // 
+            this.numericUpDownBlue.Location = new System.Drawing.Point(256, 11);
+            this.numericUpDownBlue.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownBlue.Name = "numericUpDownBlue";
+            this.numericUpDownBlue.Size = new System.Drawing.Size(41, 20);
+            this.numericUpDownBlue.TabIndex = 4;
+            // 
+            // numericUpDownGreen
+            // 
+            this.numericUpDownGreen.Location = new System.Drawing.Point(149, 11);
+            this.numericUpDownGreen.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownGreen.Name = "numericUpDownGreen";
+            this.numericUpDownGreen.Size = new System.Drawing.Size(41, 20);
+            this.numericUpDownGreen.TabIndex = 3;
+            // 
+            // numericUpDownRed
+            // 
+            this.numericUpDownRed.Location = new System.Drawing.Point(47, 11);
+            this.numericUpDownRed.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDownRed.Name = "numericUpDownRed";
+            this.numericUpDownRed.Size = new System.Drawing.Size(45, 20);
+            this.numericUpDownRed.TabIndex = 2;
+            // 
+            // labelColor
+            // 
+            this.labelColor.AutoSize = true;
+            this.labelColor.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelColor.Location = new System.Drawing.Point(10, 13);
+            this.labelColor.Name = "labelColor";
+            this.labelColor.Size = new System.Drawing.Size(31, 13);
+            this.labelColor.TabIndex = 1;
+            this.labelColor.Text = "Color";
+            // 
+            // panelCanvas
+            // 
+            this.panelCanvas.BackColor = System.Drawing.Color.White;
+            this.panelCanvas.Location = new System.Drawing.Point(3, 37);
+            this.panelCanvas.Name = "panelCanvas";
+            this.panelCanvas.Size = new System.Drawing.Size(570, 371);
+            this.panelCanvas.TabIndex = 0;
+            this.panelCanvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelCanvas_MouseMove);
+            // 
+            // radioButtonRed
+            // 
+            this.radioButtonRed.AutoSize = true;
+            this.radioButtonRed.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.radioButtonRed.Location = new System.Drawing.Point(98, 11);
+            this.radioButtonRed.Name = "radioButtonRed";
+            this.radioButtonRed.Size = new System.Drawing.Size(45, 17);
+            this.radioButtonRed.TabIndex = 5;
+            this.radioButtonRed.TabStop = true;
+            this.radioButtonRed.Text = "Red";
+            this.radioButtonRed.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonGreen
+            // 
+            this.radioButtonGreen.AutoSize = true;
+            this.radioButtonGreen.ForeColor = System.Drawing.Color.White;
+            this.radioButtonGreen.Location = new System.Drawing.Point(196, 11);
+            this.radioButtonGreen.Name = "radioButtonGreen";
+            this.radioButtonGreen.Size = new System.Drawing.Size(54, 17);
+            this.radioButtonGreen.TabIndex = 6;
+            this.radioButtonGreen.TabStop = true;
+            this.radioButtonGreen.Text = "Green";
+            this.radioButtonGreen.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonBlue
+            // 
+            this.radioButtonBlue.AutoSize = true;
+            this.radioButtonBlue.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.radioButtonBlue.Location = new System.Drawing.Point(303, 11);
+            this.radioButtonBlue.Name = "radioButtonBlue";
+            this.radioButtonBlue.Size = new System.Drawing.Size(46, 17);
+            this.radioButtonBlue.TabIndex = 7;
+            this.radioButtonBlue.TabStop = true;
+            this.radioButtonBlue.Text = "Blue";
+            this.radioButtonBlue.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 582);
+            this.Controls.Add(this.panelDrawing);
+            this.Controls.Add(this.trackBarX);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.labelConsole);
+            this.Controls.Add(this.richTextBoxConsole);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarX)).EndInit();
+            this.panelDrawing.ResumeLayout(false);
+            this.panelDrawing.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownBlue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxConsole;
+        private System.Windows.Forms.Label labelConsole;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.TrackBar trackBarX;
+        private System.Windows.Forms.Panel panelDrawing;
+        private System.Windows.Forms.Label labelColor;
+        private System.Windows.Forms.Panel panelCanvas;
+        private System.Windows.Forms.NumericUpDown numericUpDownBlue;
+        private System.Windows.Forms.NumericUpDown numericUpDownGreen;
+        private System.Windows.Forms.NumericUpDown numericUpDownRed;
+        private System.Windows.Forms.RadioButton radioButtonBlue;
+        private System.Windows.Forms.RadioButton radioButtonGreen;
+        private System.Windows.Forms.RadioButton radioButtonRed;
+    }
+}
+
---
+++ UPCode/Lab7-pad/Form1.cs
@@ -0,0 +1,92 @@
+﻿using SharpDX.DirectInput;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7_pad {
+    public partial class Form1 : Form {
+        private Joystick gamepad;
+        private int oldXVal = 32767;
+        private DirectInput directInput;
+        public Form1() {
+            InitializeComponent();
+            connectJoystick();
+            timer.Enabled = true;
+        }
+
+        private void connectJoystick() {
+            // Initialize DirectInput
+            directInput = new DirectInput();
+
+            // Find all gamepads connected to the system
+            IList<DeviceInstance> connectedGamepads = new List<DeviceInstance>();
+            // - look for gamepads
+            foreach (DeviceInstance deviceInstance in directInput.GetDevices(DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices)) {
+                connectedGamepads.Add(deviceInstance);
+            }
+            // - look for joysticks
+            foreach (DeviceInstance deviceInstance in directInput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AllDevices)) {
+                connectedGamepads.Add(deviceInstance);
+            }
+
+            // Use the gamepad found
+            if (connectedGamepads.Count >= 1) {
+                gamepad = new Joystick(directInput, connectedGamepads[0].InstanceGuid);
+                gamepad.Acquire();
+                richTextBoxConsole.AppendText("Połączono z padem: " + connectedGamepads[0].InstanceName + "\n");
+            } else {
+                richTextBoxConsole.AppendText("Nie znaleziono padów!" + "\n");
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e) {
+            //pollJoystick();
+            richTextBoxConsole.AppendText(Cursor.Position.X.ToString() + " " + Cursor.Position.Y.ToString());
+        }
+
+        private void pollJoystick() {
+            JoystickState state = new JoystickState();
+            state = gamepad.GetCurrentState();
+
+            int xVal = state.X;
+            int yVal = state.Y;
+
+            textBoxX.Text = xVal.ToString();
+            textBoxY.Text = yVal.ToString();
+
+            if (xVal < oldXVal) {
+                trackBarX.Value--;
+            } else if (xVal > oldXVal) {
+                trackBarX.Value++;
+            }
+
+            bool buttonOne = state.Buttons[0];
+            if (buttonOne) {
+                radioButtonBlue.Enabled = true;
+                richTextBoxConsole.AppendText("Red color selected" + "\n");
+            }
+
+            var buttonTwo = state.Buttons[1];
+            if (buttonTwo) {
+                radioButtonGreen.Enabled = true;
+                richTextBoxConsole.AppendText("Green color selected" + "\n");
+            }
+
+            var buttonThree = state.Buttons[2];
+            if (buttonThree) {
+                radioButtonRed.Enabled = true;
+                richTextBoxConsole.AppendText("Red color selected" + "\n");
+            }
+        }
+
+        private void panelCanvas_MouseMove(object sender, MouseEventArgs e) {
+
+        }
+    }
+}
